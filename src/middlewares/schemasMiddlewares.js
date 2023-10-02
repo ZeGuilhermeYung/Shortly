1b55836e298ea1e20e8fc42c8cf45fbf4747b181
ZeGuilhermeYung <jgryung@gmail.com>
2023-10-02 17:43:47 -0300

    urlSchemasValidation

--- src/middlewares/schemasMiddlewares.js
+++ src/middlewares/schemasMiddlewares.js
@@ -1,29 +1,5 @@
 import { baseSchemas } from "../schemas/schemas.js";
 
-async function validateUrlSchema(req, res, next) {
-    const { url } = res.locals.body;
-
-    try {
-        if (!baseSchemas[url]) {
-            return res.status(422).send({ message: "Schema 'url' does not exist" });
-        }
-    
-        const validation = baseSchemas[url].validate(req.body, { abortEarly: false });
-    
-        if (validation.error) {
-            const errors = validation.error.details.map(error => error.message);
-            return res.status(422).send({ message: errors });
-        }
-    
-        res.locals.body = req.body;
-        next();
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-    
-}
-
 function schemaValidation (validator) {
     if (!baseSchemas[validator]) {
         return `${validator} does not exist`;
@@ -42,4 +18,22 @@ function schemaValidation (validator) {
     }
 }
 
-export { validateUrlSchema, schemaValidation };
+function urlSchemaValidation (validator) {
+    if (!urlSchemas[validator]) {
+        return `${validator} does not exist`;
+    }
+
+    return async function (req, res, next) {
+        const validation = urlSchemas[validator].validate(req.body, { abortEarly: false });
+        if (validation.error) {
+            const errors = validation.error.details.map(error => error.message);
+            res.status(422).send({ message: errors });
+            return;
+        }
+    
+        res.locals.body = req.body;
+        next();
+    }
+}
+
+export { schemaValidation, urlSchemaValidation };
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { deleteUrl, openUrl, readUrl, shortUrl } from "../controllers/urlsControllers.js";
 import { authValidation } from "../middlewares/tokenMiddlewares.js";
-import { validateUrlSchema } from "../middlewares/schemasMiddlewares.js";
+import { urlSchemaValidation } from "../middlewares/schemasMiddlewares.js";
 import { nonRepeatedUrlValidation, userHasUrlValidation } from "../middlewares/urlsMiddlewares.js";
 
 const urlsRouter = express.Router();
-urlsRouter.post("/urls/shorten", authValidation, validateUrlSchema, nonRepeatedUrlValidation, shortUrl);
+urlsRouter.post("/urls/shorten", authValidation, urlSchemaValidation, nonRepeatedUrlValidation, shortUrl);
 urlsRouter.get("/urls/:id", readUrl);
 urlsRouter.get("/urls/open/:shortUrl", openUrl);
 urlsRouter.delete("/urls/:id", authValidation, userHasUrlValidation, deleteUrl);

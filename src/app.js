e8d5e400dd78c5c0171bee5e80771c39780179a3
ZeGuilhermeYung <jgryung@gmail.com>
2023-10-02 01:32:31 -0300

    correcting connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import router from "./routes/index.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+});

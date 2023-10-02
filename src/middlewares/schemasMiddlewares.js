0eef918c27e219da26a40e364996410b7a584937
ZeGuilhermeYung <jgryung@gmail.com>
2023-10-02 17:54:32 -0300

    restoring schemas

--- src/middlewares/schemasMiddlewares.js
+++ src/middlewares/schemasMiddlewares.js
@@ -18,22 +18,4 @@ function schemaValidation (validator) {
     }
 }
 
-function urlSchemaValidation (validator) {
-    if (!urlSchemas[validator]) {
-        return `${validator} does not exist`;
-    }
-
-    return async function (req, res, next) {
-        const validation = urlSchemas[validator].validate(req.body, { abortEarly: false });
-        if (validation.error) {
-            const errors = validation.error.details.map(error => error.message);
-            res.status(422).send({ message: errors });
-            return;
-        }
-    
-        res.locals.body = req.body;
-        next();
-    }
-}
-
-export { schemaValidation, urlSchemaValidation };
+export { schemaValidation };
